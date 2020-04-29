e94260f5f206ecfd6877f1e7a3d19fc973d9bea5
ciderpark <43592267+ciderpark@users.noreply.github.com>
2020-04-29 16:11:37 +0800

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -56,92 +56,6 @@ void train(shared_ptr<Darknet> net, DataLoader& loader, torch::optim::Optimizer&
 	}
 }
 
-template <typename DataLoader>
-void evaluate(shared_ptr<Darknet> net, DataLoader& loader, size_t valid_size, torch::Device device){
-	net->eval();
-	torch::NoGradGuard no_grad;
-	std::cout << "evaluating model ..." << std::endl;
-	vector<torch::Tensor> sample_metrics {};
-	vector<torch::Tensor> targets_v {};
-	for(auto& batch : loader){
-		torch::Tensor input = batch.data.to (device);
-		torch::Tensor targets = batch.target/*.to (device)*/;
-		targets = xywh2xyxy(targets);
-		targets.slice(2, 1, 5) *= yolo_ops.image_size;
-		torch::Tensor output = net->forward(input);
-		/*//
-				std::cout << "output.sizes (): " << output.sizes() << std::endl;
-				//    std::cout << output.select(2, 4).sizes() << std::endl;
-					float conf = 0.0;
-					for (int i = 0; i < output.size(1); i++) {
-						conf = output[0][i][4].item<float>();
-						if (conf > 0.0) {
-				//    std::cout << "confidence: " << conf_mask[0][i][0] << ", index: " << i << std::endl;
-							float max = 0.0;
-							int idx = 0;
-							for (int j = 5; j < output.size(2); j++) {
-								if (output[0][i][j].item<float>() > max) {
-									max = output[0][i][j].item<float>();
-									idx = j;
-								}
-							}
-				//    std::cout << "pre: " << output[0][i].max() << ", index: " << i << std::endl;
-							float confi = output[0][i][4].item<float>();
-							std::cout << "class_id: " << idx - 5 << ", pre_index: " << i
-									<< ", conf: " << confi << ", class_score: " << max
-									<< ", x:" << output[0][i][0].item<float>() << ", y: " << output[0][i][1].item<float>()
-									<< ", w: " << output[0][i][2].item<float>() << ", h: " << output[0][i][3].item<float>()
-									<< std::endl;
-						}
-					}
-				//*/
-		output = net->write_results(output, yolo_ops.num_classes, 0.6, 0.4);
-		std::cout << output << std::endl;
-//		output = output.to(device);
-//		std::cout << output.device() << std::endl;
-		sample_metrics.push_back(get_batch_statistics(output, targets, 0.2));
-		targets_v.push_back(targets);
-	}
-	std::cout << "here" << std::endl;
-	torch::Tensor true_positives, pred_scores, pred_labels;
-	bool write = false;
-	for(int i = 0; i < sample_metrics.size(); i++){
-		if(sample_metrics[i].dim() == 1)
-			continue;
-		torch::Tensor p = sample_metrics[i].select(0, 0);
-		torch::Tensor s = sample_metrics[i].select(0, 1);
-		torch::Tensor l = sample_metrics[i].select(0, 2);
-		if(!write){
-			true_positives = p;
-			pred_scores = s;
-			pred_labels = l;
-			write = true;
-		}
-		else{
-			true_positives = torch::cat({true_positives, p}, 0);
-			pred_scores = torch::cat({pred_scores, p}, 0);
-			pred_labels = torch::cat({pred_labels, p}, 0);
-		}
-	}
-	vector<int> labels {};
-	for(torch::Tensor t : targets_v){
-		for(int i = 0; i < t.size(0); i++){
-			for(int j = 0; j < t.size(1); j++){
-				if(t[i][j].sum().item<float>() == 0.0)
-					continue;
-				labels.push_back(t[i][j].select(0, 0).item<int>());
-			}
-		}
-	}
-	std::cout << true_positives.sizes() << pred_scores.sizes() << pred_labels.sizes() << std::endl;
-	std::cout << labels.size() << std::endl;
-	torch::Tensor precision, recall, AP, f1;
-	std::vector<int> ap_class;
-	std::tie(precision, recall, AP, f1, ap_class) = ap_per_class(true_positives, pred_scores, pred_labels, labels);
-	std::cout << precision << recall << AP << f1 << ap_class << std::endl;
-	std::cout << "evaluation done." << std::endl;
-}
-
 int
 main (int argc,
       char** argv)
@@ -183,8 +97,8 @@ main (int argc,
   std::cout << "Done." << std::endl;
   // Load weights
   std::cout << "loading weight ..." << endl;
-//  net->load_weights (yolo_ops.train_weights_path);
-  torch::load(net, yolo_ops.train_weights_path);
+  net->load_weights (yolo_ops.train_weights_path);
+//  torch::load(net, yolo_ops.train_weights_path);
   std::cout << "Done." << endl;
 
   net->to (device);
@@ -193,13 +107,9 @@ main (int argc,
   std::cout << "train start ..." << std::endl;
   for (size_t i = 0; i < yolo_ops.iterations; i++)
   {
-//	train(net, *train_loader, optimizer, i + 1, train_size, device);
-	if(!((i + 1) % yolo_ops.evaluation_interval)){
-		evaluate(net, *train_loader, train_size, device);
-	}
-	break;
+	train(net, *train_loader, optimizer, i + 1, train_size, device);
   }
-//  torch::save (net, "yolov3-voc2t.pt");
+  torch::save (net, "yolov3-voc2t.pt");
   std::cout << "train done." << std::endl;
   return (0);
 }
