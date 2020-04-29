60cab1e20aec01697ea66a10ef9c8edcc91d26fd
ciderpark <43592267+ciderpark@users.noreply.github.com>
2020-04-29 16:18:02 +0800

    Update utils.h

--- inc/utils.h
+++ inc/utils.h
@@ -6,25 +6,15 @@
 
 using Data = std::vector<std::pair<std::string, std::vector<std::vector<float>>>>;
 
-/*struct COCODateOps
- {
- int classes_num = 80;
- std::string train_pathes = "/home/ciderpark/eclipse-workspace/YOLO-V3_test/data/coco/trainvalno5k.txt";
- std::string valid_pathes = "/home/ciderpark/eclipse-workspace/YOLO-V3_test/data/coco/5k.txt";
- std::string classes_names = "/home/ciderpark/eclipse-workspace/YOLO-V3_test/data/coco.names";
- };
-
- static COCODateOps data_ops;*/
-
 struct yoloOps {
 	int image_size = 416;
 	int batch_size = 2;
 	int num_classes = 2;
 	std::string cfg_path =
-			"/home/ciderpark/eclipse-workspace/YOLO-V3_voc2012/cfg/yolov3.cfg";
-	std::string train_weights_path = "/home/ciderpark/eclipse-workspace/YOLO-V3_voc2012/build/yolov3-voc2t.pt";
-//			"/home/ciderpark/eclipse-workspace/YOLO-V3_voc2012/weights/darknet53.conv.74";
-	float learn_rate = 1e-5;  //defalt: 1e-3, 0.001 in cfg
+			"/path/to/your/project/cfg/yolov3.cfg";
+	std::string train_weights_path =
+			"/path/to/your/project/weights/darknet53.conv.74";
+	float learn_rate = 0.001;  //defalt: 1e-3, 0.001 in cfg
 //    float weight_decay = 0.0005;
 	int iterations = 100;
 	int log_interval = 10;
@@ -33,26 +23,15 @@ struct yoloOps {
 
 static yoloOps yolo_ops;
 
-/*struct vocOptions
- {
- std::string train_path = "/home/ciderpark/Documents/VOCdevkit/VOC2012/ImageSets/Main/train.txt";
- std::string val_path = "/home/ciderpark/Documents/VOCdevkit/VOC2012/ImageSets/Main/val.txt";
- std::string label_path = "/home/ciderpark/Documents/VOCdevkit/VOC2012/Annotations/";
- std::string img_path = "/home/ciderpark/Documents/VOCdevkit/VOC2012/JPEGImages/";
- std::string trans_label_path = "/home/ciderpark/Documents/VOCdevkit/VOC2012/TransLabels/";
- };
-
- static vocOptions voc_ops;*/
-
 struct voc2clsOptions {
 	std::string train_path =
-			"/home/ciderpark/Documents/VOCdevkit/VOC2012/2cls/train.txt";
+			"/path/to/your/dataset/VOCdevkit/VOC2012/2cls/train.txt";
 	std::string val_path =
-			"/home/ciderpark/Documents/VOCdevkit/VOC2012/2cls/val.txt";
+			"/path/to/your/dataset/VOCdevkit/VOC2012/2cls/val.txt";
 	std::string img_path =
-			"/home/ciderpark/Documents/VOCdevkit/VOC2012/JPEGImages/";
+			"/path/to/your/dataset/VOCdevkit/VOC2012/JPEGImages/";
 	std::string trans_label_path =
-			"/home/ciderpark/Documents/VOCdevkit/VOC2012/2cls/2clsLabels/";
+			"/path/to/your/dataset/VOCdevkit/VOC2012/2cls/2clsLabels/";
 };
 
 static voc2clsOptions voc2_ops;
